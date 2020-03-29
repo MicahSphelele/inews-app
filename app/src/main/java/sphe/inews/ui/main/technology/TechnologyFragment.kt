fe6ab472714325f47fec571fb59c093e691f36bb
sphelele95 <sphelele@ikhokha.com>
2020-03-29 19:35:30 +0200

    - Finished working on Technology fragment

--- app/src/main/java/sphe/inews/ui/main/technology/TechnologyFragment.kt
+++ app/src/main/java/sphe/inews/ui/main/technology/TechnologyFragment.kt
@@ -1,20 +1,120 @@
 package sphe.inews.ui.main.technology
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_sport.*
+import kotlinx.android.synthetic.main.fragment_technology.*
+import kotlinx.android.synthetic.main.fragment_technology.btn_retry
+import kotlinx.android.synthetic.main.fragment_technology.recyclerView
+import kotlinx.android.synthetic.main.fragment_technology.shimmer_view_container
+import kotlinx.android.synthetic.main.fragment_technology.txt_message
 import sphe.inews.R
+import sphe.inews.models.Article
+import sphe.inews.network.INewResource
+import sphe.inews.ui.main.adapters.ArticleAdapter
+import sphe.inews.viewmodels.ViewModelProviderFactory
+import javax.inject.Inject
 
 /**
  * A simple [Fragment] subclass.
  */
-class TechnologyFragment : Fragment() {
+class TechnologyFragment : Fragment(), ArticleAdapter.ArticleListener {
+
+    @Inject
+    lateinit var providerFactory: ViewModelProviderFactory
+
+    @Inject
+    lateinit var adapter: ArticleAdapter
+
+    private lateinit var viewModel: TechnologyViewModel
+
+    private lateinit var mainContext : Context
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_technology, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mainContext = view.context
+
+        recyclerView?.layoutManager = LinearLayoutManager(activity)
+
+        adapter.setListener(this)
+
+        viewModel = ViewModelProvider(this, providerFactory).get(TechnologyViewModel::class.java)
+
+        btn_retry.setOnClickListener {
+            this.getTechnologyNews()
+        }
+    }
+
+    override fun onArticleClicked(article: Article) {
+        Toast.makeText(mainContext,""+article.publishedAt, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getTechnologyNews(){
+        viewModel.observeTechnologyNews("za")?.removeObservers(viewLifecycleOwner)
+        viewModel.observeTechnologyNews("za")?.observe(viewLifecycleOwner, Observer { res ->
+
+            when(res.status){
+                INewResource.Status.LOADING -> {
+                    this.setButtonRetryVisibility(false)
+                    this.setTextViewMessageVisibility(false)
+                    this.setShimmerLayoutVisibility(true)
+                }
+                INewResource.Status.ERROR -> {
+                    this.setButtonRetryVisibility(true)
+                    this.setTextViewMessageVisibility(true)
+                    this.setShimmerLayoutVisibility(false)
+                }
+                INewResource.Status.SUCCESS -> {
+                    this.setButtonRetryVisibility(false)
+                    this.setTextViewMessageVisibility(false)
+                    this.setShimmerLayoutVisibility(false)
+                    recyclerView.adapter = adapter
+                    adapter.setArticles(res.data?.articles)
+                }
+
+            }
+
+        })
+    }
+
+    private fun setButtonRetryVisibility(isVisible:Boolean){
+        if(isVisible){
+            btn_retry.visibility = View.VISIBLE
+        }else{
+            btn_retry.visibility = View.GONE
+        }
+    }
+
+    private fun setTextViewMessageVisibility(isVisible:Boolean){
+        if(isVisible){
+            txt_message.visibility = View.VISIBLE
+        }else{
+            txt_message.visibility = View.GONE
+        }
+    }
+
+    private fun setShimmerLayoutVisibility(isVisible:Boolean){
+        if(isVisible){
+            shimmer_view_container.visibility = View.VISIBLE
+            shimmer_view_container.startShimmer()
+        }else{
+            shimmer_view_container.visibility = View.GONE
+            shimmer_view_container.stopShimmer()
+
+        }
     }
 
 
