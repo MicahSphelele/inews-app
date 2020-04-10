5db2047fca6df69edc77d7bc6ac2619ebef471d0
sphelele95 <sphelele@ikhokha.com>
2020-04-10 13:40:43 +0200

    - Optimized imports

--- app/src/main/java/sphe/inews/ui/main/news/sport/SportFragment.kt
+++ app/src/main/java/sphe/inews/ui/main/news/sport/SportFragment.kt
@@ -6,7 +6,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
