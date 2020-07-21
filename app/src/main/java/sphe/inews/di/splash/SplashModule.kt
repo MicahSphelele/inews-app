17ffac554c9da63043adddb50af95a9b582d4206
Sphelele <sphemicah@gmail.com>
2020-07-21 20:16:58 +0200

    - 🎁 Added app splash screen

---
+++ app/src/main/java/sphe/inews/di/splash/SplashModule.kt
@@ -0,0 +1,7 @@
+package sphe.inews.di.splash
+
+import dagger.Module
+
+@Module
+class SplashModule {
+}
---
+++ app/src/main/java/sphe/inews/di/splash/SplashScope.kt
@@ -0,0 +1,10 @@
+package sphe.inews.di.splash
+
+import javax.inject.Scope
+
+@Scope
+@MustBeDocumented
+@Retention(AnnotationRetention.RUNTIME)
+annotation class SplashScope {
+
+}
