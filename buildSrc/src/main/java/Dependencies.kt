0f831228ad7b6cbacd4b54279c33c90e9b66d155
Sphelele <sphemicah@gmail.com>
2020-09-07 19:42:54 +0200

    - 💉 Start Gradle work

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,14 @@
+//class Dependencies{
+object Versions {
+    const val kotlin = "1.4.0"
+    const val android = "4.0.1"
+}
+
+object Deps {
+
+    const val KOTLIN_STD_LIB = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    const val KOTLIN_GRADLE = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val ANDROID_TOOLS_GRADLE = "com.android.tools.build:gradle:${Versions.android}"
+}
+
+//}
