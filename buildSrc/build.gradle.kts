6532b0f553df722ec84f9eb62c52634279d9b636
Sphelele <sphemicah@gmail.com>
2020-09-07 21:45:40 +0200

    - ⚡ Updated gradle script

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,9 +4,22 @@ repositories {
 
 plugins {
   `kotlin-dsl`
+  id("name.remal.check-dependency-updates") version "1.0.211"
 }
 
+//buildscript {
+//  repositories {
+//    maven { url = uri("https://plugins.gradle.org/m2/") }
+//  }
+//  dependencies {
+//    classpath("name.remal:gradle-plugins:1.0.211")
+//  }
+//}
+//
+//apply(plugin = "name.remal.check-dependency-updates")
+
 tasks{
+
   compileKotlin{
     kotlinOptions{
       allWarningsAsErrors = true
