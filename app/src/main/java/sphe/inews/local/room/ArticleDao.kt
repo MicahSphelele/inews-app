d0e46e4cdfb0469a280cb42d2bcd7734e856f1ae
Sphelele <sphemicah@gmail.com>
2020-09-15 21:07:50 +0200

    - ✔ Added  and completed ArticleDao

---
+++ app/src/main/java/sphe/inews/local/room/ArticleDao.kt
@@ -0,0 +1,23 @@
+package sphe.inews.local.room
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import sphe.inews.models.news.Article
+import sphe.inews.util.Constants
+
+@Dao
+interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(article: Article)
+
+    @Delete
+    suspend fun delete(article: Article)
+
+    @Query("SELECT * FROM ${Constants.TABLE_ARTICLE}")
+    suspend fun getArticles() : LiveData<Article>
+
+    @Query("SELECT * FROM ${Constants.TABLE_ARTICLE} WHERE ${Constants.URL} =:url")
+    suspend fun getArticle(url : String) : Article
+
+}
