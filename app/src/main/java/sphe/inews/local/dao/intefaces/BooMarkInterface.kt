a5fd22af0b0ad522704f6d7ba8bc37dba112c434
Sphelele <sphemicah@gmail.com>
2020-09-17 18:25:02 +0200

    - ⚡ Added  BooMarkInterface execution methods to be implemented

--- app/src/main/java/sphe/inews/local/dao/intefaces/BooMarkInterface.kt
+++ app/src/main/java/sphe/inews/local/dao/intefaces/BooMarkInterface.kt
@@ -1,4 +1,15 @@
 package sphe.inews.local.dao.intefaces
 
+import androidx.lifecycle.LiveData
+import sphe.inews.models.news.Article
+
 interface BooMarkInterface {
+
+   fun insert(article: Article)
+
+   fun delete(article: Article)
+
+   fun getBooMarks() : LiveData<Article>
+
+   fun getBooMark(url : String) : Article
 }
