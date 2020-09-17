384777f1bbdc74fc302bf8d6e3a7814a04384671
Sphelele <sphemicah@gmail.com>
2020-09-17 18:34:21 +0200

    - 🎉 BookmarkRepository implements BookmarkInterface

--- app/src/main/java/sphe/inews/local/repo/BookmarkRepository.kt
+++ app/src/main/java/sphe/inews/local/repo/BookmarkRepository.kt
@@ -1,4 +1,24 @@
 package sphe.inews.local.repo
 
-class BookmarkRepository {
+import androidx.lifecycle.LiveData
+import sphe.inews.local.dao.intefaces.BookmarkInterface
+import sphe.inews.models.news.Article
+
+class BookmarkRepository : BookmarkInterface {
+
+    override fun insert(article: Article) {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(article: Article) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getBooMarks(): LiveData<Article> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getBooMark(url: String): Article {
+        TODO("Not yet implemented")
+    }
 }
