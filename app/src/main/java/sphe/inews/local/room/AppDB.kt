f85a6e1f5c3b397ec2f8e3a82940d891422313cb
Sphelele <sphemicah@gmail.com>
2020-09-17 18:38:23 +0200

    - ✨ Added bookmarkDao to AppDB Instance

--- app/src/main/java/sphe/inews/local/room/AppDB.kt
+++ app/src/main/java/sphe/inews/local/room/AppDB.kt
@@ -1,11 +1,18 @@
 package sphe.inews.local.room
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
+import sphe.inews.local.dao.BookmarkDao
+import sphe.inews.models.news.Article
 
+@Database(entities = [Article::class],version = 2,exportSchema = false)
 abstract class AppDB : RoomDatabase(){
+
+    abstract fun bookmarkDao() : BookmarkDao
+
     companion object {
 
         @Volatile
