41545a9c587bb5d325b477be48f9ef435e7be92d
Sphelele <sphemicah@gmail.com>
2020-09-20 16:39:51 +0200

    - 🎉 Made bookmark a parcelable object

--- app/src/main/java/sphe/inews/models/Bookmark.kt
+++ app/src/main/java/sphe/inews/models/Bookmark.kt
@@ -1,21 +1,24 @@
 package sphe.inews.models
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 import sphe.inews.util.Constants
 
 @Entity(tableName = Constants.TABLE_BOOKMARK)
+@Parcelize
 data class Bookmark(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = Constants.ID)
-    var id: Int,
+    var id: Int = 0,
     @ColumnInfo(name = Constants.URL)
-    var url: String,
+    var url: String = "",
     @ColumnInfo(name = Constants.AUTHOR)
-    var author: String,
+    var author: String = "'",
     @ColumnInfo(name = Constants.CONTENT)
-    var content: String,
+    var content: String = "",
     @ColumnInfo(name = Constants.DESC)
     var description: String = "",
     @ColumnInfo(name = Constants.PUBLISH_AT)
@@ -28,4 +31,4 @@ data class Bookmark(
     var title: String,
     @ColumnInfo(name = Constants.IMAGE_URL)
     var urlToImage: String = ""
-)
+) : Parcelable
