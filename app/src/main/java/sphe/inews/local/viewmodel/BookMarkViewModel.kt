75883f033c92118438590bb9a27c08edaf98fc7a
Sphelele <sphemicah@gmail.com>
2020-09-21 13:37:39 +0200

    ✔ Implemented BookmarkInterface under BookMarkViewModel

--- app/src/main/java/sphe/inews/local/viewmodel/BookMarkViewModel.kt
+++ app/src/main/java/sphe/inews/local/viewmodel/BookMarkViewModel.kt
@@ -1,7 +1,26 @@
 package sphe.inews.local.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import sphe.inews.local.dao.intefaces.BookmarkInterface
+import sphe.inews.models.Bookmark
 
-class BookMarkViewModel : ViewModel() {
+class BookMarkViewModel : ViewModel(), BookmarkInterface {
+
+    override fun insert(bookmark: Bookmark): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(bookmark: Bookmark): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getBooMarks(): LiveData<Bookmark> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getBooMark(url: String): Bookmark {
+        TODO("Not yet implemented")
+    }
 
 }
