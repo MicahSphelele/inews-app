8e2a4e4447df2138cbbe7923b7581138166968f0
Sphelele <sphemicah@gmail.com>
2020-09-23 12:06:19 +0200

    - ✔ Added a function within the extension functions

--- app/src/main/java/sphe/inews/util/Extensions.kt
+++ app/src/main/java/sphe/inews/util/Extensions.kt
@@ -1,6 +1,13 @@
 package sphe.inews.util
 
+import sphe.inews.models.Bookmark
+
 fun Any?.notNull() : Boolean {
+
+    return this != null
+}
+
+fun Bookmark?.notNull() : Boolean {
 
     return this != null
 }
