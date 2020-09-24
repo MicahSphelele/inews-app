44bb02e49c5c42e5050ab1cab97ea788ffa1ff1a
Sphelele <sphemicah@gmail.com>
2020-09-24 17:22:37 +0200

    - 🛰 Created a NetworkReceiver to handle network or wifi issue

---
+++ app/src/main/java/sphe/inews/util/networkmanager/NetworkReceiver.kt
@@ -0,0 +1,12 @@
+package sphe.inews.util.networkmanager
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class NetworkReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
