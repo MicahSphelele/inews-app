7d837ae05f54e4b0aaabff35302318c83cbec6b4
Sphelele <sphemicah@gmail.com>
2021-02-17 23:20:02 +0200

    - Completed AboutDialogFragment ViewBinding ✨

--- app/src/main/java/sphe/inews/ui/main/dialogfragments/AboutDialogFragment.kt
+++ app/src/main/java/sphe/inews/ui/main/dialogfragments/AboutDialogFragment.kt
@@ -19,15 +19,12 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.fragment_about.*
-import kotlinx.android.synthetic.main.fragment_about.toolbar
-import kotlinx.android.synthetic.main.fragment_about.txt_app_version
-import kotlinx.android.synthetic.main.fragment_about_2.card
-import kotlinx.android.synthetic.main.fragment_about_2.layout_about
 import sphe.inews.R
+import sphe.inews.databinding.FragmentAboutBinding
 import sphe.inews.util.Constants
 import javax.inject.Inject
 import javax.inject.Named
+
 @AndroidEntryPoint
 class AboutDialogFragment : BottomSheetDialogFragment() {
 
@@ -43,7 +40,9 @@ class AboutDialogFragment : BottomSheetDialogFragment() {
     @Named(Constants.NAMED_SET_NEW)
     lateinit var constraintSetNew: ConstraintSet
 
-    private var altLayout:Boolean = false
+    private var altLayout: Boolean = false
+
+    private lateinit var binding: FragmentAboutBinding
 
     @Suppress("RedundantOverride")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,64 +51,81 @@ class AboutDialogFragment : BottomSheetDialogFragment() {
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dialog:Dialog = super.onCreateDialog(savedInstanceState)
+        val dialog: Dialog = super.onCreateDialog(savedInstanceState)
         dialog.setOnShowListener {
             val bottomSheetDialog = it as BottomSheetDialog
-            setupFullHeight(bottomSheetDialog,BottomSheetBehavior.STATE_EXPANDED)
+            setupFullHeight(bottomSheetDialog, BottomSheetBehavior.STATE_EXPANDED)
         }
         dialog.setOnDismissListener {
             val bottomSheetDialog = it as BottomSheetDialog
-            setupFullHeight(bottomSheetDialog,BottomSheetBehavior.STATE_COLLAPSED)
+            setupFullHeight(bottomSheetDialog, BottomSheetBehavior.STATE_COLLAPSED)
         }
         return dialog
     }
 
-    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
         return inflater.inflate(R.layout.fragment_about, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        (toolbar as Toolbar).navigationIcon = ContextCompat.getDrawable(requireContext(),R.drawable.ic_action_home)
+        binding = FragmentAboutBinding.bind(view)
 
-        (toolbar as Toolbar).setNavigationOnClickListener{
+        (binding.toolbar as Toolbar).navigationIcon =
+            ContextCompat.getDrawable(requireContext(), R.drawable.ic_action_home)
+
+        (binding.toolbar as Toolbar).setNavigationOnClickListener {
             dismiss()
         }
 
-        constraintSetOld.clone(layout_about)
-        constraintSetNew.clone(context,R.layout.fragment_about_2)
-        
-        card.setOnClickListener {
+        constraintSetOld.clone(binding.layoutAbout)
+        constraintSetNew.clone(context, R.layout.fragment_about_2)
+
+        binding.toolbar.setOnClickListener {
             performLayoutTransition()
         }
         //Github
-        card_btn_1.setOnClickListener {
-            Constants.launchCustomTabIntent(activity as Activity,resources.getString(R.string.url_github))
+        binding.cardBtn1.setOnClickListener {
+            Constants.launchCustomTabIntent(
+                activity as Activity,
+                resources.getString(R.string.url_github)
+            )
         }
         //Linkedin
-        card_btn_2.setOnClickListener {
-            Constants.launchCustomTabIntent(activity as Activity,resources.getString(R.string.url_linkedin))
+        binding.cardBtn2.setOnClickListener {
+            Constants.launchCustomTabIntent(
+                activity as Activity,
+                resources.getString(R.string.url_linkedin)
+            )
         }
         //Gmail
-        card_btn_3.setOnClickListener {
+        binding.cardBtn3.setOnClickListener {
             val intent = Intent(Intent.ACTION_SENDTO)
-            intent.type="text/plain"
+            intent.type = "text/plain"
             intent.data = Uri.parse("mailto:${resources.getString(R.string.url_gmail)}")
-            intent.putExtra(Intent.EXTRA_EMAIL,resources.getString(R.string.url_gmail))
-            intent.putExtra(Intent.EXTRA_SUBJECT,"Hi Sphelele")
-            if(intent.resolveActivity(requireActivity().packageManager)!=null){
+            intent.putExtra(Intent.EXTRA_EMAIL, resources.getString(R.string.url_gmail))
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Hi Sphelele")
+            if (intent.resolveActivity(requireActivity().packageManager) != null) {
                 startActivity(intent)
-            }else{
+            } else {
                 Toast.makeText(activity, "Application not found", Toast.LENGTH_SHORT).show()
             }
         }
         //Instagram
-        card_btn_4.setOnClickListener {
-            Constants.launchCustomTabIntent(activity as Activity,resources.getString(R.string.url_instagram))
+        binding.cardBtn4.setOnClickListener {
+            Constants.launchCustomTabIntent(
+                activity as Activity,
+                resources.getString(R.string.url_instagram)
+            )
         }
 
-        txt_app_version.text = appVersion
+        binding.txtAppVersion.text = appVersion
 
     }
 
@@ -124,6 +140,7 @@ class AboutDialogFragment : BottomSheetDialogFragment() {
 //        }
 
     }
+
     @Suppress("RedundantOverride")
     override fun onActivityCreated(args: Bundle?) {
         super.onActivityCreated(args)
@@ -132,29 +149,30 @@ class AboutDialogFragment : BottomSheetDialogFragment() {
 //        }
     }
 
-    private fun setupFullHeight(bottomSheetDialog: BottomSheetDialog, state : Int){
-        val bottomSheet:FrameLayout = bottomSheetDialog.findViewById<FrameLayout>(R.id.design_bottom_sheet) as FrameLayout
+    private fun setupFullHeight(bottomSheetDialog: BottomSheetDialog, state: Int) {
+        val bottomSheet: FrameLayout =
+            bottomSheetDialog.findViewById<FrameLayout>(R.id.design_bottom_sheet) as FrameLayout
         val behavior = BottomSheetBehavior.from(bottomSheet)
         val layoutParams = bottomSheet.layoutParams
-        val windowHeight:Int = getWindowHeight()
+        val windowHeight: Int = getWindowHeight()
         layoutParams.height = windowHeight
         bottomSheet.layoutParams = layoutParams
         behavior.state = state
     }
 
-    private fun getWindowHeight() : Int{
+    private fun getWindowHeight(): Int {
         val displayMetrics = DisplayMetrics()
         activity?.windowManager?.defaultDisplay?.getMetrics(displayMetrics)
         return displayMetrics.heightPixels
     }
 
-    private fun performLayoutTransition(){
-        TransitionManager.beginDelayedTransition(layout_about)
-        altLayout = if(!altLayout){
-            constraintSetNew.applyTo(layout_about)
+    private fun performLayoutTransition() {
+        TransitionManager.beginDelayedTransition(binding.layoutAbout)
+        altLayout = if (!altLayout) {
+            constraintSetNew.applyTo(binding.layoutAbout)
             true
-        }else{
-            constraintSetOld.applyTo(layout_about)
+        } else {
+            constraintSetOld.applyTo(binding.layoutAbout)
             false
         }
     }
