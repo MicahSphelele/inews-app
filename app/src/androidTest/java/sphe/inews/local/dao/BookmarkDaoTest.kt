3ac2e9c67f230034f60bd3171e73fd670468c688
Sphelele <sphemicah@gmail.com>
2021-07-05 09:15:48 +0200

    - Added tests for LiveData in room 👌🏾💻
    - Added testDBGetBooMarksObservedISEmptyAndNotNull
    - Added testDBGetBooMarksObservedISNotNullOrEmpty

--- app/src/androidTest/java/sphe/inews/local/dao/BookmarkDaoTest.kt
+++ app/src/androidTest/java/sphe/inews/local/dao/BookmarkDaoTest.kt
@@ -4,15 +4,23 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
+import org.junit.FixMethodOrder
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runners.MethodSorters
+import sphe.inews.di.AppModule
+import sphe.inews.getOrAwaitValue
 import sphe.inews.local.room.AppDB
 import sphe.inews.models.Bookmark
 
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@UninstallModules(AppModule::class)
+@HiltAndroidTest
 class BookmarkDaoTest {
 
     @get:Rule
@@ -131,6 +139,48 @@ class BookmarkDaoTest {
         val bookmark = dao.getBooMark(bookMarkEntry.url!!)
 
         assertThat(bookmark).isEqualTo(bookMarkEntry)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testDBGetBooMarksObservedISEmptyAndNotNull() = runBlockingTest {
+        val bookmarks = dao.getBooMarksObserved().getOrAwaitValue()
+        assertThat(bookmarks).isEmpty()
+        assertThat(bookmarks).isNotNull()
+        assertThat(bookmarks.size).isEqualTo(0)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testDBGetBooMarksObservedISNotNullOrEmpty() = runBlockingTest {
+        val bookMarkEntry1 = Bookmark(
+            id = 1,
+            url = "https://www.news24.com/sport/soccer/euro2020/france-fall-out-adrien-rabiots-mother-clashes-with-mbappe-pogba-families-after-euro-exit-20210630",
+            author = "sport",
+            content = "In sensational scenes, France midfielder Adrien Rabiot's mother was shown in video footage clashing with the Mbappe and Pogba families after their shock Euro 2020 exit. Didier Deschamp's side were… [+2150 chars]",
+            description = "In sensational scenes, France midfielder Adrien Rabiot's mother was shown in video footage clashing with the Mbappe and Pogba families after their shock Euro 2020 exit.",
+            publishedAt = "2021-06-30T09:12:30Z",
+            sourceId = "news24",
+            sourceName = "News24",
+            title = "France fall -out | Adrien Rabiot's mother clashes with Mbappe, Pogba families after Euro exit - News24",
+            urlToImage = "https://cdn.24.co.za/files/Cms/General/d/4032/780cff14f0bf419eb2414c9baa69ea19.jpg",
+            category="sports")
+        val bookMarkEntry2 = Bookmark(id = 2,
+            url = "https://www.news24.com/sport/soccer/euro2020/france-fall-out-adrien-rabiots-mother-clashes-with-mbappe-pogba-families-after-euro-exit-20210630, author=sport, content=In sensational scenes, France midfielder Adrien Rabiot's mother was shown in video footage clashing with the Mbappe and Pogba families after their shock Euro 2020 exit.",
+            author = "sport",
+            content = "Didier Deschamp's side were… [+2150 chars], description=In sensational scenes, France midfielder Adrien Rabiot's mother was shown in video footage clashing with the Mbappe and Pogba families after their shock Euro 2020 exit",
+            publishedAt = "2021-06-30T09:12:30Z",
+            sourceId = "news24",
+            sourceName = "News24",
+            title = "France fall-out | Adrien Rabiot's mother clashes with Mbappe, Pogba families after Euro exit - News24",
+            urlToImage = "https://cdn.24.co.za/files/Cms/General/d/4032/780cff14f0bf419eb2414c9baa69ea19.jpg, category=sports")
+        dao.insert(bookMarkEntry1)
+        dao.insert(bookMarkEntry2)
+
+        val bookmarks = dao.getBooMarksObserved().getOrAwaitValue()
+        assertThat(bookmarks).isNotNull()
+        assertThat(bookmarks).isNotEmpty()
+        assertThat(bookmarks.size).isEqualTo(2)
     }
 
     fun tearDown() {
