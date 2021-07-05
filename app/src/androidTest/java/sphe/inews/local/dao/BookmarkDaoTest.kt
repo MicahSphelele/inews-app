9395724f809b30afd2df648ba493d993f691ffe2
Sphelele <sphemicah@gmail.com>
2021-07-05 09:01:39 +0200

    - Added testDBGetSingleBookmark 💻 👌🏾

--- app/src/androidTest/java/sphe/inews/local/dao/BookmarkDaoTest.kt
+++ app/src/androidTest/java/sphe/inews/local/dao/BookmarkDaoTest.kt
@@ -109,7 +109,30 @@ class BookmarkDaoTest {
         assertThat(bookmarks.size).isEqualTo(2)
         assertThat(bookmarks).contains(bookMarkEntry1)
     }
-    
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testDBGetSingleBookmark() = runBlockingTest {
+        val bookMarkEntry = Bookmark(
+            id = 1,
+            url = "https://www.news24.com/sport/soccer/euro2020/france-fall-out-adrien-rabiots-mother-clashes-with-mbappe-pogba-families-after-euro-exit-20210630",
+            author = "sport",
+            content = "In sensational scenes, France midfielder Adrien Rabiot's mother was shown in video footage clashing with the Mbappe and Pogba families after their shock Euro 2020 exit. Didier Deschamp's side were… [+2150 chars]",
+            description = "In sensational scenes, France midfielder Adrien Rabiot's mother was shown in video footage clashing with the Mbappe and Pogba families after their shock Euro 2020 exit.",
+            publishedAt = "2021-06-30T09:12:30Z",
+            sourceId = "news24",
+            sourceName = "News24",
+            title = "France fall -out | Adrien Rabiot's mother clashes with Mbappe, Pogba families after Euro exit - News24",
+            urlToImage = "https://cdn.24.co.za/files/Cms/General/d/4032/780cff14f0bf419eb2414c9baa69ea19.jpg",
+            category="sports")
+
+        dao.insert(bookMarkEntry)
+
+        val bookmark = dao.getBooMark(bookMarkEntry.url!!)
+
+        assertThat(bookmark).isEqualTo(bookMarkEntry)
+    }
+
     fun tearDown() {
         database.close()
     }
