be350f51b246e11c666658633b4c8f4a7ceb4489
Sphelele <sphemicah@gmail.com>
2021-07-20 13:58:39 +0200

    - Added NewsCategory  💻👌🏾

---
+++ app/src/main/java/sphe/inews/models/enums/NewsCategory.kt
@@ -0,0 +1,9 @@
+package sphe.inews.models.enums
+
+enum class NewsCategory(name: String) {
+    BUSINESS ("business"),
+    HEALTH("health"),
+    SPORTS("sports"),
+    TECHNOLOGY("technology"),
+    ENTERTAINMENT("entertainment")
+}
