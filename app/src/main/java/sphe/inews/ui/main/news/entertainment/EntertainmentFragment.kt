b4bb0368beb95f71ec5e6586ca0591c1fa75f24f
Sphelele <sphemicah@gmail.com>
2021-07-20 13:26:32 +0200

    - Removed EntertainmentViewModel 🛢

--- app/src/main/java/sphe/inews/ui/main/news/entertainment/EntertainmentFragment.kt
+++ app/src/main/java/sphe/inews/ui/main/news/entertainment/EntertainmentFragment.kt
@@ -2,11 +2,11 @@ package sphe.inews.ui.main.news.entertainment
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.transition.MaterialElevationScale
@@ -15,13 +15,15 @@ import sphe.inews.R
 import sphe.inews.databinding.FragmentEntertainmentBinding
 import sphe.inews.models.Bookmark
 import sphe.inews.models.news.Article
-import sphe.inews.network.Resources
+import sphe.inews.models.news.NewsResponse
+import sphe.inews.models.response.NetworkResult
 import sphe.inews.ui.BaseActivity
 import sphe.inews.ui.main.adapters.ArticleAdapter
 import sphe.inews.ui.main.dialogfragments.ArticlePreviewFragment
 import sphe.inews.ui.main.dialogfragments.ViewYoutubeDialogFragment
 import sphe.inews.util.Constants
 import sphe.inews.util.notNull
+import sphe.inews.viewmodels.NewsViewModel
 import javax.inject.Inject
 
 /**
@@ -30,14 +32,14 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class EntertainmentFragment : Fragment(R.layout.fragment_entertainment), ArticleAdapter.ArticleListener {
 
-    lateinit var viewYoutubeDialogFragment: ViewYoutubeDialogFragment
+    private lateinit var viewYoutubeDialogFragment: ViewYoutubeDialogFragment
+    private lateinit var binding: FragmentEntertainmentBinding
+
+    private val newsViewModel by viewModels<NewsViewModel>()
+    private var liveData: LiveData<NetworkResult<NewsResponse>> = MediatorLiveData()
 
     @Inject
     lateinit var adapter: ArticleAdapter
-
-    private val viewModel by viewModels<EntertainmentViewModel>()
-
-    private lateinit var binding: FragmentEntertainmentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -124,40 +126,32 @@ class EntertainmentFragment : Fragment(R.layout.fragment_entertainment), Article
     }
 
     private fun getEntertainmentNews() {
-        viewModel.observeEntertainmentNews("za")?.let {
-            viewModel.observeEntertainmentNews("za")?.removeObservers(this)
-            viewModel.observeEntertainmentNews("za")?.observe(viewLifecycleOwner, { res ->
-
-                when (res.status) {
-                    Resources.Status.LOADING -> {
-                        this.setErrorViewsVisibility(false)
-                        this.setShimmerLayoutVisibility(true)
-                    }
-                    Resources.Status.ERROR -> {
-                        this.setErrorViewsVisibility(true)
-                        this.setShimmerLayoutVisibility(false)
-
-                        context?.resources?.let {
-                            binding.txtMessage.text = context?.resources?.getString(R.string.msg_error)
-                        }
-                    }
-                    Resources.Status.SUCCESS -> {
-                        this.setErrorViewsVisibility(false)
-                        this.setShimmerLayoutVisibility(false)
-
-                        binding.recyclerView.adapter = adapter
-                        res.data?.let {
-                            adapter.setArticles(res.data.articles)
-                        }
-
+        liveData = newsViewModel.getNews("za", Constants.ENTERTAINMENT)
+        liveData.removeObservers(viewLifecycleOwner)
+        liveData.observe(viewLifecycleOwner) {
+            when(it) {
+                is NetworkResult.Loading -> {
+                    setErrorViewsVisibility(false)
+                    setShimmerLayoutVisibility(true)
+                }
+                is NetworkResult.Error -> {
+                    setErrorViewsVisibility(true)
+                    setShimmerLayoutVisibility(false)
+                    context?.resources?.let {
+                        binding.txtMessage.text = context?.resources?.getString(R.string.msg_error)
                     }
                 }
-
-            })
+                is NetworkResult.Success -> {
+                    setErrorViewsVisibility(false)
+                    setShimmerLayoutVisibility(false)
+                    binding.recyclerView.adapter = adapter
+                    it.data?.let { results ->
+                        adapter.setArticles(results.articles)
+                    }
+                }
+            }
         }
-
     }
-
 
     private fun setErrorViewsVisibility(isVisible: Boolean) {
         if (isVisible) {
