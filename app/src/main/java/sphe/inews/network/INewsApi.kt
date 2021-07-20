f4d21f3bbc32eb7539514781268740d1e9bcf884
Sphelele <sphemicah@gmail.com>
2021-07-20 13:16:44 +0200

    - Created new viewmodel 💻
    - Created NewsViewModel

--- app/src/main/java/sphe/inews/network/INewsApi.kt
+++ app/src/main/java/sphe/inews/network/INewsApi.kt
@@ -1,6 +1,7 @@
 package sphe.inews.network
 
 import io.reactivex.Flowable
+import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 import retrofit2.http.Query
 import sphe.inews.models.news.NewsResponse
@@ -9,18 +10,39 @@ import sphe.inews.util.Constants
 interface INewsApi {
 
     @GET("top-headlines?category=${Constants.BUSINESS}")//business
-    fun getBusinessNews(@Query("country")country: String, @Query("apiKey") apiKey :String): Flowable<NewsResponse>?
+    fun getBusinessNews(
+        @Query("country") country: String,
+        @Query("apiKey") apiKey: String
+    ): Flowable<NewsResponse>?
 
     @GET("top-headlines?category=${Constants.HEALTH}")//health
-    fun getHealthNews(@Query("country") country: String, @Query("apiKey") apiKey :String): Flowable<NewsResponse>?
+    fun getHealthNews(
+        @Query("country") country: String,
+        @Query("apiKey") apiKey: String
+    ): Flowable<NewsResponse>?
 
     @GET("top-headlines?category=${Constants.SPORTS}")//sports
-    fun getSportNews(@Query("country") country: String, @Query("apiKey") apiKey :String): Flowable<NewsResponse>?
+    fun getSportNews(
+        @Query("country") country: String,
+        @Query("apiKey") apiKey: String
+    ): Flowable<NewsResponse>?
 
     @GET("top-headlines?category=${Constants.TECHNOLOGY}")//technology
-    fun getTechNews(@Query("country") country: String, @Query("apiKey") apiKey :String): Flowable<NewsResponse>?
+    fun getTechNews(
+        @Query("country") country: String,
+        @Query("apiKey") apiKey: String
+    ): Flowable<NewsResponse>?
 
     @GET("top-headlines?category=${Constants.ENTERTAINMENT}")//entertainment
-    fun getEntertainmentNews(@Query("country") country: String, @Query("apiKey") apiKey :String): Flowable<NewsResponse>?
+    fun getEntertainmentNews(
+        @Query("country") country: String,
+        @Query("apiKey") apiKey: String
+    ): Flowable<NewsResponse>?
 
+    @GET("top-headlines")
+    suspend fun getNews(
+        @Query("country") country: String,
+        @Query("category") category: String,
+        @Query("apiKey") apiKey: String
+    ) : NewsResponse
 }
---
+++ app/src/main/java/sphe/inews/viewmodels/NewsViewModel.kt
@@ -0,0 +1,47 @@
+package sphe.inews.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import sphe.inews.models.news.NewsResponse
+import sphe.inews.models.response.NetworkResult
+import sphe.inews.network.INewsApi
+import sphe.inews.util.Constants
+import javax.inject.Inject
+
+@HiltViewModel
+class NewsViewModel @Inject constructor(private var newsApi: INewsApi) : ViewModel() {
+
+   private var newsResponse: MediatorLiveData<NetworkResult<NewsResponse>> = MediatorLiveData()
+
+   private fun executeNewsFlow(country: String, category: String) : Flow<NewsResponse> = flow {
+        emit(newsApi.getNews(country, category, String(Constants.PACKS_NEWS)))
+    }
+
+    fun getNews(country: String, category: String) : LiveData<NetworkResult<NewsResponse>>{
+        viewModelScope.launch {
+            try {
+                executeNewsFlow(country, category)
+                    .flowOn(Dispatchers.IO)
+                    .onStart {
+                        newsResponse.value = NetworkResult.Loading()
+                    }.map {
+                        it
+                    }.collect {
+                        newsResponse.value = NetworkResult.Success(data = it)
+                    }
+            } catch (ex: Exception) {
+                newsResponse.value = NetworkResult.Error(
+                    message = "Something went wrong: ${ex.message}",
+                    data = null
+                )
+            }
+        }
+        return newsResponse
+    }
+}
