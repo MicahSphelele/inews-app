6a4b5546ba7a123f9ef984f5826a3a350b85bd35
Sphelele <sphemicah@gmail.com>
2021-07-21 16:53:05 +0200

    - Removed unused code on api calls 🛢

--- app/src/main/java/sphe/inews/network/Covid19Api.kt
+++ app/src/main/java/sphe/inews/network/Covid19Api.kt
@@ -7,7 +7,8 @@ import sphe.inews.models.covid.CovidResponse
 
 interface Covid19Api {
 
-
     @GET("latest_stat_by_country.php")//latest_stat_by_country_name.php
     fun getCovidStatsByCountry(@Query("country") country: String): Flowable<CovidResponse>?
+    @GET("latest_stat_by_country.php")
+    suspend fun getCountryCovidStats(@Query("country") country: String): CovidResponse
 }
--- app/src/main/java/sphe/inews/network/INewsApi.kt
+++ app/src/main/java/sphe/inews/network/INewsApi.kt
@@ -1,43 +1,10 @@
 package sphe.inews.network
 
-import io.reactivex.Flowable
-import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 import retrofit2.http.Query
 import sphe.inews.models.news.NewsResponse
-import sphe.inews.util.Constants
 
 interface INewsApi {
-
-    @GET("top-headlines?category=${Constants.BUSINESS}")//business
-    fun getBusinessNews(
-        @Query("country") country: String,
-        @Query("apiKey") apiKey: String
-    ): Flowable<NewsResponse>?
-
-    @GET("top-headlines?category=${Constants.HEALTH}")//health
-    fun getHealthNews(
-        @Query("country") country: String,
-        @Query("apiKey") apiKey: String
-    ): Flowable<NewsResponse>?
-
-    @GET("top-headlines?category=${Constants.SPORTS}")//sports
-    fun getSportNews(
-        @Query("country") country: String,
-        @Query("apiKey") apiKey: String
-    ): Flowable<NewsResponse>?
-
-    @GET("top-headlines?category=${Constants.TECHNOLOGY}")//technology
-    fun getTechNews(
-        @Query("country") country: String,
-        @Query("apiKey") apiKey: String
-    ): Flowable<NewsResponse>?
-
-    @GET("top-headlines?category=${Constants.ENTERTAINMENT}")//entertainment
-    fun getEntertainmentNews(
-        @Query("country") country: String,
-        @Query("apiKey") apiKey: String
-    ): Flowable<NewsResponse>?
 
     @GET("top-headlines")
     suspend fun getNews(
