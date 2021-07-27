50998531c43cf131abf27177f6d6acfe861858ec
Sphelele <sphemicah@gmail.com>
2021-07-27 14:41:40 +0200

    - Added bookmark fragment 💻

---
+++ app/src/main/java/sphe/inews/ui/main/bookmark/BookmarkFragment.kt
@@ -0,0 +1,27 @@
+package sphe.inews.ui.main.bookmark
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.transition.MaterialElevationScale
+import sphe.inews.R
+import sphe.inews.databinding.FragmentBookmarkBinding
+import sphe.inews.databinding.FragmentViewArticleBinding
+
+class BookmarkFragment : Fragment(R.layout.fragment_bookmark) {
+
+    private lateinit var binding: FragmentBookmarkBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enterTransition = MaterialElevationScale(/* growing= */ true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentBookmarkBinding.bind(view)
+    }
+}
