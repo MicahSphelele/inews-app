c5dd48834800b64ad87bfde5842ca5f561699704
Sphelele <sphemicah@gmail.com>
2021-07-30 10:48:25 +0200

    - Updated MainActivity 💻

--- app/src/main/java/sphe/inews/ui/main/MainActivity.kt
+++ app/src/main/java/sphe/inews/ui/main/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
