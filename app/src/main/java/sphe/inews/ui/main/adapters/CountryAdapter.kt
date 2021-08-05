d5e3d632d500bd44e151525a2fd91bcf10f63760
Sphelele <sphemicah@gmail.com>
2021-08-05 14:00:06 +0200

    - Used viewHolderItemBinding on CountryAdapter  💻

--- app/src/main/java/sphe/inews/ui/main/adapters/CountryAdapter.kt
+++ app/src/main/java/sphe/inews/ui/main/adapters/CountryAdapter.kt
@@ -1,29 +1,22 @@
 package sphe.inews.ui.main.adapters
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.NonNull
 import androidx.core.content.ContextCompat
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import sphe.inews.R
 import sphe.inews.databinding.ItemCountryBinding
 import sphe.inews.models.Country
+import sphe.inews.util.viewHolderItemBinding
 
 class CountryAdapter(private var list: List<Country>) : RecyclerView.Adapter<CountryAdapter.ViewHolder>(){
 
     private lateinit var listener: CountryListener
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val viewBinder: ItemCountryBinding = DataBindingUtil.inflate(
-            LayoutInflater.from(parent.context),
-            R.layout.item_country,
-            parent,
-            false
-        )
-        return  ViewHolder(viewBinder)
+        return  ViewHolder(parent.viewHolderItemBinding(R.layout.item_country) as ItemCountryBinding)
     }
 
     override fun getItemCount(): Int {
