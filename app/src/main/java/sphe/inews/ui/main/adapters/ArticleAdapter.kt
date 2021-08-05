379a5cadbbd2d3c332a823fcd2b941b9877b187f
Sphelele <sphemicah@gmail.com>
2021-08-05 15:05:26 +0200

    - Completed data binding on  ArticleAdapter  💻

--- app/src/main/java/sphe/inews/ui/main/adapters/ArticleAdapter.kt
+++ app/src/main/java/sphe/inews/ui/main/adapters/ArticleAdapter.kt
@@ -1,19 +1,16 @@
 package sphe.inews.ui.main.adapters
 
-import android.net.Uri
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
-import android.widget.TextView
 import androidx.annotation.NonNull
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.google.android.material.imageview.ShapeableImageView
 import sphe.inews.R
 import sphe.inews.databinding.ItemArticleBinding
 import sphe.inews.models.news.Article
+import sphe.inews.util.isYoutubeBoolean
 import sphe.inews.util.viewHolderItemBinding
 
 class ArticleAdapter : RecyclerView.Adapter<ArticleAdapter.ViewHolder>() {
@@ -26,56 +23,25 @@ class ArticleAdapter : RecyclerView.Adapter<ArticleAdapter.ViewHolder>() {
         return ViewHolder(parent.viewHolderItemBinding(R.layout.item_article) as ItemArticleBinding)
     }
 
-    override fun getItemCount(): Int {
-        return list.size
-    }
+    override fun getItemCount(): Int = list.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val article = list[position]
-        val isVideo: Boolean
 
-        @Suppress("SENSELESS_COMPARISON")
-        if (article.urlToImage != null) {
-            Glide.with(holder.itemView.context)
-                .load(Uri.parse(article.urlToImage))
-                .placeholder(R.drawable.logo)
-                .error(R.drawable.logo)
-                .into(holder.image)
-        }
-
-        holder.source.text = article.source.name
-
-        val arrayTitle = article.title.split("-")
-
-        if (arrayTitle.size > 1) {
-            holder.title.text = article.title.split("-")[0]
-        } else {
-            holder.title.text = article.title
-        }
-
-        if (article.source.name == "Youtube.com") {
-
-            holder.youtube.visibility = View.VISIBLE
-            isVideo = true
-
-        } else {
-            holder.youtube.visibility = View.GONE
-            isVideo = false
-        }
+        holder.viewBinder.article = article
 
         holder.image.setOnClickListener {
-            listener.onArticleItemClick(article, isVideo)
+            listener.onArticleItemClick(article, isYoutubeBoolean(article.source.name))
         }
 
         holder.youtube.setOnClickListener {
-            listener.onArticleItemClick(article, isVideo)
+            listener.onArticleItemClick(article, isYoutubeBoolean(article.source.name))
         }
 
         holder.share.setOnClickListener {
             listener.onShareItemClick(article)
         }
-
     }
 
     fun setArticles(list: List<Article>?) {
@@ -91,14 +57,11 @@ class ArticleAdapter : RecyclerView.Adapter<ArticleAdapter.ViewHolder>() {
         setArticles(mutableListOf())
     }
 
-    class ViewHolder(@NonNull viewBinder: ItemArticleBinding) :
+    class ViewHolder(@NonNull val viewBinder: ItemArticleBinding) :
         RecyclerView.ViewHolder(viewBinder.root) {
         var image: ShapeableImageView = viewBinder.image
-        var title: TextView = viewBinder.title
-        var source: TextView = viewBinder.source
         var share: ImageButton = viewBinder.share
         var youtube: ImageButton = viewBinder.youtube
-
     }
 
     private val diffCallBack = object : DiffUtil.ItemCallback<Article>() {
@@ -109,7 +72,6 @@ class ArticleAdapter : RecyclerView.Adapter<ArticleAdapter.ViewHolder>() {
         override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
             return oldItem == newItem
         }
-
     }
 
     private val asyncListDiffer = AsyncListDiffer(this, diffCallBack)
--- app/src/main/java/sphe/inews/util/Extensions.kt
+++ app/src/main/java/sphe/inews/util/Extensions.kt
@@ -1,20 +1,26 @@
 package sphe.inews.util
 
 import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.bumptech.glide.Glide
+import com.google.android.material.imageview.ShapeableImageView
 import sphe.inews.R
 import sphe.inews.models.Bookmark
 
-fun Any?.notNull() : Boolean {
+
+fun Any?.notNull(): Boolean {
 
     return this != null
 }
 
-fun Bookmark?.notNull() : Boolean {
+fun Bookmark?.notNull(): Boolean {
 
     return this != null
 }
@@ -27,7 +33,7 @@ fun Context.showShortToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
 
-fun ViewGroup.viewHolderItemBinding(resId: Int) : ViewDataBinding {
+fun ViewGroup.viewHolderItemBinding(resId: Int): ViewDataBinding {
     return DataBindingUtil.inflate(
         LayoutInflater.from(this.context),
         R.layout.item_article,
@@ -35,3 +41,29 @@ fun ViewGroup.viewHolderItemBinding(resId: Int) : ViewDataBinding {
         false
     )
 }
+
+fun String.shrinkString(): String {
+    val arrayTitle = this.split("-")
+    return if (arrayTitle.size > 1) {
+        arrayTitle[0]
+    } else {
+        this
+    }
+}
+
+fun isYoutubeInt(string: String): Int {
+    return if (string == "Youtube.com") {
+        View.VISIBLE
+    } else {
+        View.GONE
+    }
+}
+
+fun isYoutubeBoolean(string: String): Boolean = (string == "Youtube.com")
+
+@BindingAdapter("imageUrl")
+fun loadImage(view: ShapeableImageView, url: String) = Glide.with(view.context)
+    .load(Uri.parse(url))
+    .placeholder(R.drawable.logo)
+    .error(R.drawable.logo)
+    .into(view)
