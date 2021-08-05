8aab362932c9b484115a83c93f46611589bed69e
Sphelele <sphemicah@gmail.com>
2021-08-05 13:57:54 +0200

    - Cleaned   ArticleAdapter  🧹

--- app/src/main/java/sphe/inews/ui/main/adapters/ArticleAdapter.kt
+++ app/src/main/java/sphe/inews/ui/main/adapters/ArticleAdapter.kt
@@ -1,13 +1,11 @@
 package sphe.inews.ui.main.adapters
 
 import android.net.Uri
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.annotation.NonNull
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
