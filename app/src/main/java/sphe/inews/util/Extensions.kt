046d088666d01b9c3c898c94ded71046dbc00c04
Sphelele <sphemicah@gmail.com>
2021-08-05 13:55:14 +0200

    - Added a ViewGroup extension  🔌

--- app/src/main/java/sphe/inews/util/Extensions.kt
+++ app/src/main/java/sphe/inews/util/Extensions.kt
@@ -1,7 +1,12 @@
 package sphe.inews.util
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import sphe.inews.R
 import sphe.inews.models.Bookmark
 
 fun Any?.notNull() : Boolean {
@@ -20,4 +25,13 @@ fun Context.showLongToast(message: String) {
 
 fun Context.showShortToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun ViewGroup.viewHolderItemBinding(resId: Int) : ViewDataBinding {
+    return DataBindingUtil.inflate(
+        LayoutInflater.from(this.context),
+        R.layout.item_article,
+        this,
+        false
+    )
 }
