b9fe994bf3e534e49e1c6270c6130bb5bc2a3b46
Sphelele <sphemicah@gmail.com>
2021-08-06 15:52:11 +0200

    - Implemented full data binding on country adapter  💻 🔌

--- app/src/main/java/sphe/inews/ui/main/adapters/CountryAdapter.kt
+++ app/src/main/java/sphe/inews/ui/main/adapters/CountryAdapter.kt
@@ -1,22 +1,20 @@
 package sphe.inews.ui.main.adapters
 
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.annotation.NonNull
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import sphe.inews.R
 import sphe.inews.databinding.ItemCountryBinding
 import sphe.inews.models.Country
 import sphe.inews.util.viewHolderItemBinding
 
-class CountryAdapter(private var list: List<Country>) : RecyclerView.Adapter<CountryAdapter.ViewHolder>(){
+class CountryAdapter(private var list: List<Country>) :
+    RecyclerView.Adapter<CountryAdapter.ViewHolder>() {
 
     private lateinit var listener: CountryListener
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return  ViewHolder(parent.viewHolderItemBinding(R.layout.item_country) as ItemCountryBinding)
+        return ViewHolder(parent.viewHolderItemBinding(R.layout.item_country) as ItemCountryBinding)
     }
 
     override fun getItemCount(): Int {
@@ -24,27 +22,24 @@ class CountryAdapter(private var list: List<Country>) : RecyclerView.Adapter<Cou
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
         val country = list[position]
 
-        holder.image.setImageDrawable(ContextCompat.getDrawable(holder.itemView.context,country.image))
-
-        holder.title.text = country.countryName
+        holder.viewBinder.country = country
 
         holder.itemView.setOnClickListener {
             listener.onItemCountryClick(country)
         }
     }
 
-    fun setCountryClickListener(listener: CountryListener){
+    fun setCountryClickListener(listener: CountryListener) {
         this.listener = listener
     }
 
-     class ViewHolder(@NonNull viewBinder: ItemCountryBinding): RecyclerView.ViewHolder(viewBinder.root){
-        var image : ImageView = viewBinder.image
-        var title :TextView  = viewBinder.title
-    }
+    class ViewHolder(@NonNull val viewBinder: ItemCountryBinding) :
+        RecyclerView.ViewHolder(viewBinder.root)
 
-    interface CountryListener{
+    interface CountryListener {
         fun onItemCountryClick(country: Country)
     }
 }
