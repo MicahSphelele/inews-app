5f0e935a6b384d6a0b4730e02567e6f9576363eb
Sphelele <sphemicah@gmail.com>
2021-08-06 16:02:57 +0200

    - Implemented full data binding on category adapter  🔌

--- app/src/main/java/sphe/inews/ui/main/adapters/CategoryAdapter.kt
+++ app/src/main/java/sphe/inews/ui/main/adapters/CategoryAdapter.kt
@@ -3,7 +3,6 @@ package sphe.inews.ui.main.adapters
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.annotation.NonNull
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.imageview.ShapeableImageView
 import sphe.inews.R
@@ -11,18 +10,20 @@ import sphe.inews.databinding.ItemCategoryBinding
 import sphe.inews.enums.NewsCategory
 import sphe.inews.util.viewHolderItemBinding
 
-class CategoryAdapter(private var list: Array<NewsCategory>) : RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
+class CategoryAdapter(private var list: Array<NewsCategory>) :
+    RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
 
     private lateinit var listener: CategoryListener
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-       return ViewHolder(parent.viewHolderItemBinding(R.layout.item_category) as ItemCategoryBinding)
+        return ViewHolder(parent.viewHolderItemBinding(R.layout.item_category) as ItemCategoryBinding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val category = list[position]
-        holder.image.setImageDrawable(ContextCompat.getDrawable(holder.itemView.context,category.image))
-        holder.title.text = category.title
+
+        holder.viewBinder.newsCategory = category
+
         holder.itemView.setOnClickListener {
             listener.onCategoryItemClick(category)
         }
@@ -32,12 +33,13 @@ class CategoryAdapter(private var list: Array<NewsCategory>) : RecyclerView.Adap
         return list.size
     }
 
-    class ViewHolder(@NonNull viewBinder: ItemCategoryBinding) : RecyclerView.ViewHolder(viewBinder.root) {
+    class ViewHolder(@NonNull val viewBinder: ItemCategoryBinding) :
+        RecyclerView.ViewHolder(viewBinder.root) {
         var image: ShapeableImageView = viewBinder.image
         var title: TextView = viewBinder.title
     }
 
-    fun setCountryClickListener(listener: CategoryListener){
+    fun setCountryClickListener(listener: CategoryListener) {
         this.listener = listener
     }
 
