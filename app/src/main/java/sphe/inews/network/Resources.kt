f5747ff67446332aa98b6f42c0ec631e0ed46684
Sphelele <sphemicah@gmail.com>
2021-08-14 17:37:27 +0200

    - Clean code  🧹

--- app/src/main/java/sphe/inews/network/Resources.kt
+++ app/src/main/java/sphe/inews/network/Resources.kt
@@ -10,13 +10,15 @@ data class Resources<T>(val status: Status, val data: T?, val message: String?) 
     }
 
     companion object {
-        fun <T> success(@Nullable data:T):Resources<T> {
+        fun <T> success(@Nullable data: T): Resources<T> {
             return Resources(Status.SUCCESS, data, null)
         }
-        fun <T> error(@NonNull msg:String, @Nullable data:T?):Resources<T> {
+
+        fun <T> error(@NonNull msg: String, @Nullable data: T?): Resources<T> {
             return Resources<T>(Status.ERROR, data, msg)
         }
-        fun <T> loading(@Nullable data:T):Resources<T> {
+
+        fun <T> loading(@Nullable data: T): Resources<T> {
             return Resources<T>(Status.LOADING, data, null)
         }
     }
