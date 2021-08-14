bb9555dacc9e69e904690e3911e13439325ecf23
Sphelele <sphemicah@gmail.com>
2021-08-14 17:33:04 +0200

    - Clean code 🧹

--- app/src/main/java/sphe/inews/local/dao/BookmarkDao.kt
+++ app/src/main/java/sphe/inews/local/dao/BookmarkDao.kt
@@ -9,21 +9,21 @@ import sphe.inews.util.Constants
 interface BookmarkDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(bookmark: Bookmark) : Long
+    suspend fun insert(bookmark: Bookmark): Long
 
     @Delete
-    suspend fun delete(bookmark: Bookmark) : Int
-
-     @Query("SELECT * FROM ${Constants.TABLE_BOOKMARK}")
-     fun getBooMarksObserved() : LiveData<List<Bookmark>>
+    suspend fun delete(bookmark: Bookmark): Int
 
     @Query("SELECT * FROM ${Constants.TABLE_BOOKMARK}")
-    suspend fun getBooMarks() : List<Bookmark>
+    fun getBooMarksObserved(): LiveData<List<Bookmark>>
+
+    @Query("SELECT * FROM ${Constants.TABLE_BOOKMARK}")
+    suspend fun getBooMarks(): List<Bookmark>
 
     @Query("SELECT * FROM ${Constants.TABLE_BOOKMARK} WHERE ${Constants.URL} = :url")
-    suspend fun getBooMark(url : String) : Bookmark
+    suspend fun getBooMark(url: String): Bookmark
 
     @Query("SELECT * FROM ${Constants.TABLE_BOOKMARK} WHERE ${Constants.CATEGORY} = :category")
-    suspend fun getBooMarksByCategory(category: String) : List<Bookmark>
+    suspend fun getBooMarksByCategory(category: String): List<Bookmark>
 
 }
