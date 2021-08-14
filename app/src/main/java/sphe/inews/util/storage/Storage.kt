2fcbf8e1146e1d27236d99abf2aa4df5d14fbaba
Sphelele <sphemicah@gmail.com>
2021-08-14 17:43:51 +0200

    - Clean code  🧹

--- app/src/main/java/sphe/inews/util/storage/Storage.kt
+++ app/src/main/java/sphe/inews/util/storage/Storage.kt
@@ -2,7 +2,7 @@ package sphe.inews.util.storage
 
 interface Storage {
 
-    fun saveStringData(key:String,data:String)
+    fun saveStringData(key: String, data: String)
 
-    fun getStringData(key:String,default: String = "") : String
+    fun getStringData(key: String, default: String = ""): String
 }
