014163ef0fd08019f633f5815ec03d876eff8dce
Sphelele <sphemicah@gmail.com>
2021-08-14 17:36:27 +0200

    - Clean code 🧹

--- app/src/main/java/sphe/inews/network/response/NetworkResult.kt
+++ app/src/main/java/sphe/inews/network/response/NetworkResult.kt
@@ -2,8 +2,8 @@ package sphe.inews.network.response
 
 sealed class NetworkResult<T>(val data: T? = null, val message: String? = null) {
 
-   open class Success<T>(data: T) : NetworkResult<T>(data)
-   open class Error<T>(message: String?, data: T? = null) : NetworkResult<T>(data, message)
-   open class Loading<T> : NetworkResult<T>()
+    open class Success<T>(data: T) : NetworkResult<T>(data)
+    open class Error<T>(message: String?, data: T? = null) : NetworkResult<T>(data, message)
+    open class Loading<T> : NetworkResult<T>()
 
 }
