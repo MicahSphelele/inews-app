ca18ede0ad2e9882e742ef3ff2fd03d77d59be8c
Sphelele <sphemicah@gmail.com>
2021-08-14 17:45:08 +0200

    - Clean code  🧹

--- app/src/main/java/sphe/inews/viewmodels/ViewModelProviderFactory.kt
+++ app/src/main/java/sphe/inews/viewmodels/ViewModelProviderFactory.kt
@@ -5,7 +5,8 @@ import androidx.lifecycle.ViewModelProvider
 import javax.inject.Inject
 import javax.inject.Provider
 
-class ViewModelProviderFactory @Inject constructor(private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>) : ViewModelProvider.Factory {
+class ViewModelProviderFactory @Inject constructor(private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>) :
+    ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
