5c40f2d4e3c5e7f10d6f4ba0344b0347c5f4e213
Sphelele <sphemicah@gmail.com>
2021-08-18 15:08:59 +0200

    - Added location permission in the app manifest  📌

---
+++ app/src/main/java/sphe/inews/util/LocationUtils.kt
@@ -0,0 +1,35 @@
+package sphe.inews.util
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.LocationManager
+import androidx.core.app.ActivityCompat
+import java.util.*
+
+object LocationUtils {
+
+    fun isGPSOpen(context: Context): Boolean {
+        val manager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return manager.isProviderEnabled(LocationManager.GPS_PROVIDER) || manager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER)
+    }
+
+    fun isLocationPermissionEnabled(context: Context): Boolean {
+        return (ActivityCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    fun getLocationAddress(context: Context, latitude: Double, longitude: Double): Address {
+        val geocoder = Geocoder(context, Locale.getDefault())
+        val addressList = geocoder.getFromLocation(latitude, longitude, 1) as ArrayList<Address>
+        return addressList[0]
+    }
+}
