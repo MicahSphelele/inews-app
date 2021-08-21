b09c6acd9a8ca3fd7fe4f063cf914770f4c67dfe
Sphelele <sphemicah@gmail.com>
2021-08-21 13:30:07 +0200

    - Added provideWeatherApi in  AppModule  💻

--- app/src/main/java/sphe/inews/di/NetworkModule.kt
+++ app/src/main/java/sphe/inews/di/NetworkModule.kt
@@ -7,6 +7,7 @@ import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import sphe.inews.network.Covid19Api
 import sphe.inews.network.INewsApi
+import sphe.inews.network.WeatherApi
 import sphe.inews.util.Constants
 import javax.inject.Named
 
@@ -22,5 +23,10 @@ object NetworkModule {
     @Provides
     fun provideCovid19Api(@Named(Constants.NAMED_COVID_19) retrofit: Retrofit) : Covid19Api {
         return retrofit.create(Covid19Api::class.java)
+    }
+
+    @Provides
+    fun provideWeatherApi(@Named(Constants.NAMED_WEATHER_API) retrofit: Retrofit) : WeatherApi {
+        return retrofit.create(WeatherApi::class.java)
     }
 }
