ba6a000ffe4d0d72cffdf912c7641150781c14f0
Sphelele <sphemicah@gmail.com>
2021-08-21 13:27:29 +0200

    - Created forecast day class  💻

---
+++ app/src/main/java/sphe/inews/models/weather/forecast/ForecastDay.kt
@@ -0,0 +1,12 @@
+package sphe.inews.models.weather.forecast
+
+import com.google.gson.annotations.SerializedName
+
+data class ForecastDay(
+    val astro: Astro,
+    val date: String,
+    @SerializedName("date_epoch")
+    val dateEPOCH: Int,
+    val day: Day,
+    val hour: List<Hour>
+)
