261b8ed76bd61b5f4bfba46df48d6a8460f3a3cc
Sphelele <sphemicah@gmail.com>
2021-08-21 13:26:58 +0200

    - Created location class 💻

---
+++ app/src/main/java/sphe/inews/models/weather/Location.kt
@@ -0,0 +1,18 @@
+package sphe.inews.models.weather
+
+import com.google.gson.annotations.SerializedName
+
+data class Location(
+    val country: String,
+    @SerializedName("lat")
+    val latitude: Double,
+    val localtime: String,
+    @SerializedName("lat")
+    val localTimeInEPOCH: Int,
+    @SerializedName("lon")
+    val longitude: Double,
+    val name: String,
+    val region: String,
+    @SerializedName("tz_id")
+    val timeZoneId: String
+)
