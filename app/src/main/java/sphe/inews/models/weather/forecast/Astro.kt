19e6d204400fecc3609093e478c8b398d7a21b9f
Sphelele <sphemicah@gmail.com>
2021-08-21 13:25:58 +0200

    - Created astro class 💻

---
+++ app/src/main/java/sphe/inews/models/weather/forecast/Astro.kt
@@ -0,0 +1,16 @@
+package sphe.inews.models.weather.forecast
+
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+
+@Keep
+data class Astro(
+    @SerializedName("moon_illumination")
+    val moonIllumination: String,
+    val moon_phase: String,
+    val moonrise: String,
+    @SerializedName("moonset")
+    val moonSet: String,
+    val sunrise: String,
+    val sunset: String
+)
