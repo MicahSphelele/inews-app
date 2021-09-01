933aaf9779f739d70eaa8a5fcd8598919b88f169
Sphelele <sphemicah@gmail.com>
2021-09-01 09:14:39 +0200

    - Used weather packs  💻

--- app/src/main/java/sphe/inews/network/WeatherApi.kt
+++ app/src/main/java/sphe/inews/network/WeatherApi.kt
@@ -5,21 +5,22 @@ import retrofit2.http.Query
 
 import sphe.inews.models.weather.current.CurrentWeatherResponse
 import sphe.inews.models.weather.forecast.WeatherForecastResponse
+import sphe.inews.util.Constants
 
 interface WeatherApi {
 
     @GET("current.json")
     suspend fun getCurrentWeather(
-        @Query("key") key: String,
+        @Query("key") key: String = String(Constants.PACKS_WEATHER),
         @Query("q") city: String,
         @Query("aqi") aqi: String? = "no"
     ): CurrentWeatherResponse
 
     @GET("forecast.json")
     suspend fun getWeatherForecast(
-        @Query("key") key: String,
+        @Query("key") key: String = String(Constants.PACKS_WEATHER),
         @Query("q") city: String,
-        @Query("days") days: Int = 1,
+        @Query("days") days: Int = 5,
         @Query("aqi") aqi: String = "no",
         @Query("alerts") alerts: String = "no"
     ): WeatherForecastResponse
