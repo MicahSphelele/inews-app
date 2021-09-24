574f2e1120cf04e9b9abe8240a3d971d144a2a43
Sphelele <sphemicah@gmail.com>
2021-09-24 20:22:38 +0200

    - Continue working with use cases  💻

--- app/src/main/java/sphe/inews/domain/usecases/weather/GetCurrentWeatherUseCase.kt
+++ app/src/main/java/sphe/inews/domain/usecases/weather/GetCurrentWeatherUseCase.kt
@@ -1,8 +1,15 @@
 package sphe.inews.domain.usecases.weather
 
+import kotlinx.coroutines.CoroutineScope
 import sphe.inews.domain.repository.WeatherRepository
 import javax.inject.Inject
 
 class GetCurrentWeatherUseCase @Inject constructor(weatherRepository: WeatherRepository) {
+
+    operator fun invoke(scope: CoroutineScope,
+                        city: String,
+                        aqi: String? = "no") {
+        
+    }
 
 }
