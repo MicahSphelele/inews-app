649d089930b10cd5f05bc485f3b186e020fd1c9d
Sphelele <sphemicah@gmail.com>
2021-09-30 12:48:57 +0200

    - Completed GetCurrentWeatherUseCase  📦

--- app/src/main/java/sphe/inews/domain/usecases/weather/GetCurrentWeatherUseCase.kt
+++ app/src/main/java/sphe/inews/domain/usecases/weather/GetCurrentWeatherUseCase.kt
@@ -1,15 +1,21 @@
 package sphe.inews.domain.usecases.weather
 
+import androidx.lifecycle.LiveData
 import kotlinx.coroutines.CoroutineScope
+import sphe.inews.domain.NetworkResult
+import sphe.inews.domain.models.weather.current.CurrentWeatherResponse
 import sphe.inews.domain.repository.WeatherRepository
 import javax.inject.Inject
 
-class GetCurrentWeatherUseCase @Inject constructor(weatherRepository: WeatherRepository) {
+class GetCurrentWeatherUseCase @Inject constructor(private val weatherRepository: WeatherRepository) {
 
-    operator fun invoke(scope: CoroutineScope,
-                        city: String,
-                        aqi: String? = "no") {
-        
+    operator fun invoke(
+        scope: CoroutineScope,
+        city: String,
+        aqi: String? = "no"
+    ) : LiveData<NetworkResult<CurrentWeatherResponse>>
+    {
+        return weatherRepository.getCurrentWeather(scope, city = city, aqi = aqi)
     }
 
 }
