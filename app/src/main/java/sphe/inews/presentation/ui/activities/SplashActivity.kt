5aef68ea28670da1732eafa008e9e5e765976d0d
Sphelele <sphemicah@gmail.com>
2022-05-08 13:12:28 +0200

    - Made some changes  💻

--- app/src/main/java/sphe/inews/presentation/ui/activities/SplashActivity.kt
+++ app/src/main/java/sphe/inews/presentation/ui/activities/SplashActivity.kt
@@ -10,6 +10,7 @@ import android.view.WindowManager
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import dagger.hilt.android.AndroidEntryPoint
+import sphe.inews.BuildConfig
 import sphe.inews.R
 import sphe.inews.databinding.ActivitySplashBinding
 import sphe.inews.domain.enums.AppTheme
@@ -63,7 +64,7 @@ class SplashActivity : BaseActivity() {
             }
         }
 
-        binding.appVersion = appVersion
+        binding.appVersion = BuildConfig.TEST_STRING
 
     }
 
