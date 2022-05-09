0f5db4dad423cbcf8765f81b3d6013fce90f08f0
Sphelele <sphemicah@gmail.com>
2022-05-09 09:02:30 +0200

    Update set_local_properties.sh

--- .github/workflows/set_local_properties.sh
+++ .github/workflows/set_local_properties.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 touch local.properties
-echo "KEY_STORE=\\"$1\\"" >> local.properties
-echo "KEY_STORE_ALIAS=\\"$2\\"" >> local.properties
-echo "KEY_STORE_PASS=\\"$3\\"" >> local.properties
-echo "TEST_STRING=\\"$4\\"" >> local.properties
+
+echo "KEY_STORE=\\"$1\\"" >> ./local.properties
+echo "KEY_STORE_ALIAS=\\"$2\\"" >> ./local.properties
+echo "KEY_STORE_PASS=\\"$3\\"" >> ./local.properties
+echo "TEST_STRING=\\"$4\\"" >> ./local.properties
